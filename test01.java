d5a6852a23360719c811f4b7447243843222b26b
zhangzheng <970136399@qq.com>
2020-03-24 18:09:56 +0800

    第一次提交

---
+++ test01.java
@@ -0,0 +1,24 @@
+/**   
+ * Copyright © 2020 公司名. All rights reserved.
+ * 
+ * @Title: test01.java 
+ * @Prject: test_3.24
+ * @Package: com.bw.test 
+ * @Description: TODO
+ * @author: lenovo   
+ * @date: 2020年3月24日 下午5:56:06 
+ * @version: V1.0   
+ */
+package com.bw.test;
+
+/** 
+ * @ClassName: test01 
+ * @Description: TODO
+ * @author: lenovo
+ * @date: 2020年3月24日 下午5:56:06  
+ */
+public class test01 {
+	public static void main(String[] args) {
+		System.out.println("hello Word");
+	}
+}
